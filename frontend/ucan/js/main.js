1b7418b56cbe6d11d3a27690964f3a565778cbda
mik <mikail@ant.ms>
2016-07-20 11:59:01 +0300

    ucan dash

--- frontend/ucan/js/main.js
+++ frontend/ucan/js/main.js
@@ -10,4 +10,28 @@
 		$logo.toggleClass('hided');
 	});
 
+	$('.dash-form select').styler();
+
+	$('.progress-items-list li').on('click', function(){
+		if ($(this).hasClass('opened'))
+		{
+			$(this).removeClass('opened');
+			$(this).find('.progress-loaders').slideUp();
+		}
+		else
+		{
+			$(this).addClass('opened');
+			$(this).find('.progress-loaders').slideDown();
+		}
+	});
+
+	$('.progress-items-list li.opened .progress-loaders').slideDown(0);
+
+	$('.popup-container').on('click', function(e){
+		if (e.target == this)
+		{
+			$(this).removeClass('opened');
+		}
+	});
+
 });
