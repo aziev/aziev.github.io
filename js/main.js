75593417ea082a3f3e6e5e956a552f079f4c3bbd
Mikail <mikail@ant.ms>
2017-02-18 23:25:45 +0300

    update header

--- js/main.js
+++ js/main.js
@@ -1,4 +1,15 @@
-;$(document).ready(function(){
+$(document).ready(function() {
 
-	$('.item').on('click', function(){});
+    var $body = $('body');
+    var $header = $('.header');
+
+	$('.item').on('click', function() {});
+
+    $(document).on('scroll', function() {
+        if ($body.scrollTop() < $(window).height()) {
+            var scrolledPercents = $body.scrollTop() * 100 / $(window).height();
+            var opacity = (100 - scrolledPercents) / 100;
+            $header.css('opacity', opacity);
+        }
+    });
 });
