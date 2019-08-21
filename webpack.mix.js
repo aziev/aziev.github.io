1e1aa329faa5a274f11c151ced7645952b5d409f
aziev <mikail.aziev@gmail.com>
2019-08-21 18:43:26 +0300

    use laravel mix instead of gulp

---
+++ webpack.mix.js
@@ -0,0 +1,21 @@
+const mix = require('laravel-mix');
+
+/*
+ |--------------------------------------------------------------------------
+ | Mix Asset Management
+ |--------------------------------------------------------------------------
+ |
+ | Mix provides a clean, fluent API for defining some Webpack build steps
+ | for your Laravel application. By default, we are compiling the Sass
+ | file for the application as well as bundling up all the JS files.
+ |
+ */
+
+mix.sass('resources/sass/app.scss', 'css')
+    .options({
+        processCssUrls: false,
+        hmrOptions: {
+            host: 'localhost',
+            port: '8088'
+        },
+    });
