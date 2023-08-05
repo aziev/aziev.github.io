f3c734a3516e38b057d8df238f4079b3a8e32bf3
aziev <mikail.aziev@gmail.com>
2023-08-05 10:42:06 +0300

    migrate to vite

---
+++ src/main.js
@@ -0,0 +1,19 @@
+import { createApp } from 'vue'
+import './scss/app.scss'
+import App from './App.vue'
+
+// createApp(App).mount('#app')
+
+document.addEventListener('DOMContentLoaded', () => {
+  const height = window.innerHeight
+  const header = document.querySelector('.header')
+
+  document.addEventListener('scroll', () => {
+    const scrollY = window.scrollY
+
+    if (scrollY < height) {
+      console.log(scrollY / height, Math.pow(scrollY / height, 0.5))
+      header.style.opacity = 1 - Math.pow(scrollY / height, 0.5)
+    }
+  })
+})
